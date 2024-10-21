ad669eb35c4bd77c49fb5dae983e13031839dd6c
mohamedhamdy9966 <mh7008002@gmail.com>
2024-10-21 06:09:30 +0300

    add

--- src/components/mywork/MyWork.jsx
+++ src/components/mywork/MyWork.jsx
@@ -20,7 +20,7 @@ const MyWork = () => {
         })}
       </div>
       <div className="myWork-showMore">
-        <a href="https://github.com/mohamedhamdy9966" className="showMoreLink"><p>Show More</p></a>
+        <a href="https://github.com/mohamedhamdy9966" target="_blank" className="showMoreLink"><p>Show More</p></a>
         <img src={arrow_icon} alt="arrow-icon" />
       </div>
     </div>
